eaa8c942ea443eb1cc778c9c05e8e9518443cabb
leiwen <657106593@qq.com>
2021-03-02 23:49:49 +0800

    feat:修改样式

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,261 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Go-Nuxt
-      </h1>
-      <div class="links">
-        <ul>
-          <li>
-            <nuxt-link :to="{name:'index'}">
-              Home
-            </nuxt-link>
+    <div class="content">
+      <div class="content-left">
+        <article class="isgood public-style">
+          <div class="isgood-header">
+            <p class="title">科技信息</p>
+          </div>
+          <div class="isgood-box">
+            <div class="isgood-pic clearfix">
+              <h3><a href="/article/detail-187.html" target="_blank" title="春运抢票火热进行中，12306客户端为何一再强退">春运抢票火热进行中，12306客户端为何一再强退</a></h3>
+              <a href="/article/detail-187.html" target="_blank">
+                <img src="https://www.lxtkj.cn/public/images/default.png" width="90" height="60" alt="春运抢票火热进行中，12306客户端为何一再强退">
+              </a>
+              <p>随着春运的开始，一年一度的抢票大战又拉开帷幕，有网友爆料...<span class="red"><a href="/article/detail-187.html" target="_blank">[详细]</a></span>
+              </p>
+            </div>
+                <ul class="isgood-ul">
+                  <li class="isgood-li"> <a href="/article/detail-186.html">快手起诉快手快影侵权，要求赔偿100万元</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-185.html">苹果股价暴跌，然而这或许只是一个开始</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-182.html">高通与苹果在华专利纠纷，iPhone手机在国内市场禁售</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-174.html">淘宝退款崩了！难道是剁手党提起刀下不了手后悔了？</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-152.html">中国正建造价值10亿超导计算机 港媒：或改变世界</a> </li>
+                </ul>
+          </div>
+        </article>
+
+        <article class="isgood public-style">
+          <div class="isgood-header">
+            <p class="title">科技信息</p>
+          </div>
+          <div class="isgood-box">
+            <div class="isgood-pic clearfix">
+              <h3><a href="/article/detail-187.html" target="_blank" title="春运抢票火热进行中，12306客户端为何一再强退">春运抢票火热进行中，12306客户端为何一再强退</a></h3>
+              <a href="/article/detail-187.html" target="_blank">
+                <img src="https://www.lxtkj.cn/public/images/default.png" width="90" height="60" alt="春运抢票火热进行中，12306客户端为何一再强退">
+              </a>
+              <p>随着春运的开始，一年一度的抢票大战又拉开帷幕，有网友爆料...<span class="red"><a href="/article/detail-187.html" target="_blank">[详细]</a></span>
+              </p>
+            </div>
+                <ul class="isgood-ul">
+                  <li class="isgood-li"> <a href="/article/detail-186.html">快手起诉快手快影侵权，要求赔偿100万元</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-185.html">苹果股价暴跌，然而这或许只是一个开始</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-182.html">高通与苹果在华专利纠纷，iPhone手机在国内市场禁售</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-174.html">淘宝退款崩了！难道是剁手党提起刀下不了手后悔了？</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-152.html">中国正建造价值10亿超导计算机 港媒：或改变世界</a> </li>
+                </ul>
+          </div>
+        </article>
+
+        <article class="isgood public-style">
+          <div class="isgood-header">
+            <p class="title">科技信息</p>
+          </div>
+          <div class="isgood-box">
+            <div class="isgood-pic clearfix">
+              <h3><a href="/article/detail-187.html" target="_blank" title="春运抢票火热进行中，12306客户端为何一再强退">春运抢票火热进行中，12306客户端为何一再强退</a></h3>
+              <a href="/article/detail-187.html" target="_blank">
+                <img src="https://www.lxtkj.cn/public/images/default.png" width="90" height="60" alt="春运抢票火热进行中，12306客户端为何一再强退">
+              </a>
+              <p>随着春运的开始，一年一度的抢票大战又拉开帷幕，有网友爆料...<span class="red"><a href="/article/detail-187.html" target="_blank">[详细]</a></span>
+              </p>
+            </div>
+                <ul class="isgood-ul">
+                  <li class="isgood-li"> <a href="/article/detail-186.html">快手起诉快手快影侵权，要求赔偿100万元</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-185.html">苹果股价暴跌，然而这或许只是一个开始</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-182.html">高通与苹果在华专利纠纷，iPhone手机在国内市场禁售</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-174.html">淘宝退款崩了！难道是剁手党提起刀下不了手后悔了？</a> </li>
+                  <li class="isgood-li"> <a href="/article/detail-152.html">中国正建造价值10亿超导计算机 港媒：或改变世界</a> </li>
+                </ul>
+          </div>
+        </article>
+      </div>
+      <div class="content-right">
+        <div class="right-con items">
+          <ul class="items-content">
+            <li class="items-title">
+              <a href="/article/detail-184.html" target="_blank">支付宝刷脸支付产品 "蜻蜓"，即将开启支付新时代</a>
+            </li>
+            <li>
+              <a href="/article/detail-182.html" target="_blank">高通与苹果在华专利纠纷，iPhone手机在国内市场禁售</a>
+            </li>
+            <li>
+              <a href="/article/detail-175.html" target="_blank">王者荣耀为什么在上分最后一把总容易遇到坑B？</a>
+            </li>
+          </ul>
+        <div class="clear"></div>
+          <ul class="items-content">
+            <li class="items-title">
+              <a href="/article/detail-172.html" target="_blank">小米8系列手机惊艳亮相，加入新配色和透明版</a>
+            </li>
+            <li>
+              <a href="/article/detail-165.html" target="_blank">2018新iPhone手机iPhone XS、iPhone XR全球售价公布</a>
+            </li>
+            <li>
+              <a href="/article/detail-163.html" target="_blank">命，不是廉价品——扁鹊进阶攻略</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="right-con news">
+          <ul class="news-content">
+            <li class="news-title">
+              <a href="/article/detail-188.html" target="_blank">为何很多人看不起指数基金定投但又跑不赢它？</a>
+            </li>
+            <li>
+              <a href="/article/detail-187.html" target="_blank">春运抢票火热进行中，12306客户端为何一再强退</a>
+            </li>
+            <li>
+              <a href="/article/detail-186.html" target="_blank">快手起诉快手快影侵权，要求赔偿100万元</a>
+            </li>
+            <li>
+              <a href="/article/detail-185.html" target="_blank">苹果股价暴跌，然而这或许只是一个开始</a>
+            </li>
+            <li>
+              <a href="/article/detail-184.html" target="_blank">支付宝刷脸支付产品 "蜻蜓"，即将开启支付新时代</a>
+            </li>
+          </ul>
+        <div class="clear"></div>
+        <ul class="news-content">
+          <li class="news-title">
+            <a href="/article/detail-183.html" target="_blank">未来几年VR产品对生活的影响</a>
           </li>
           <li>
-            <nuxt-link :to="{name:'article'}">
-              Article
-            </nuxt-link>
+            <a href="/article/detail-182.html" target="_blank">高通与苹果在华专利纠纷，iPhone手机在国内市场禁售</a>
           </li>
           <li>
-            <nuxt-link :to="{name:'article-detail',params:{id:3226}}">
-              Detail
-            </nuxt-link>
+            <a href="/article/detail-181.html" target="_blank">家里的电视都能中病毒你信吗？</a>
           </li>
           <li>
-            <nuxt-link :to="{name:'about'}">
-              About
-            </nuxt-link>
+            <a href="/article/detail-180.html" target="_blank">寒潮来袭 数据告诉你威力有多大？</a>
           </li>
           <li>
-            <nuxt-link :to="{name:'asyncData'}">
-              asyncData
-            </nuxt-link>
+            <a href="/article/detail-179.html" target="_blank">Win10鲜为人知的好功能 你没用过的云剪贴板</a>
           </li>
         </ul>
+        </div>
+
+        <div class="right-con news public-style" style="border-bottom:none;">
+        <div class="isgood-header">
+          <p class="title">业界资讯</p>
+        </div>
+        <ul class="news-content">
+                      <li class="news-title">
+                    <a href="/article/detail-72.html" target="_blank">奥佳华按摩椅OG-7608震撼登场 科技感十足</a>
+          </li>
+                     <li>
+                    <a href="/article/detail-71.html" target="_blank">小身材大作为 解读电装机器人的前世今生</a>
+          </li>
+                     <li>
+                    <a href="/article/detail-70.html" target="_blank">目标督办顺应时代，北京明途抢占高地</a>
+          </li>
+                     <li>
+                    <a href="/article/detail-69.html" target="_blank">少年时代的“超跑梦”，如今已走进现实</a>
+          </li>
+                     <li>
+                    <a href="/article/detail-68.html" target="_blank">会发光隐形墨水研制成功</a>
+          </li>
+                     <li>
+                    <a href="/article/detail-67.html" target="_blank">首届智博会重庆开幕，BOE画屏为生活添彩</a>
+          </li>
+                 </ul>
+      </div>
+      </div>
+
+      <div class="content-aside">
+      <!--焦点图-->
+      <article class="carousel">
+        <div class="section-focus-pic" id="section-focus-pic">
+          <div class="pages" data-scro="list">
+            <ul>
+              <li class="item" style="left: 0px;">
+                <a href="/article/detail-184.html" target="_blank"><img src="https://www.lxtkj.cn/uploads/picture/20181215/7de5137cbd480ac4a226e79bdb85ce31.jpg" width="335" height="200" alt="支付宝刷脸支付产品 即将开启支付新时代"></a>
+                <h3><a href="/article/detail-184.html" target="_blank" style="color:#FFF;text-decoration:none;">支付宝刷脸支付产品 "蜻蜓"，即将开启支付新时代</a></h3>
+                <div></div>
+              </li>
+              <li class="item">
+                <a href="/article/detail-183.html" target="_blank"><img src="https://www.lxtkj.cn/uploads/picture/20181215/3f4791065efcc361a66ffe221f19d4e9.jpg" width="335" height="200" alt="未来几年VR产品对生活的影响"></a>
+                <h3><a href="/article/detail-183.html" target="_blank" style="color:#FFF;text-decoration:none;">未来几年VR产品对生活的影响</a></h3>
+                <div></div>
+              </li>
+              <li class="item">
+                <a href="/article/detail-182.html" target="_blank"><img src="https://www.lxtkj.cn/uploads/picture/20181215/7e00c1cf5660a148121828fd72dd4521.jpg" width="335" height="200" alt="高通与苹果在华专利纠纷，iPhone手机在国内市场禁售"></a>
+                <h3><a href="/article/detail-182.html" target="_blank" style="color:#FFF;text-decoration:none;">高通与苹果在华专利纠纷，iPhone手机在国内市场禁售</a></h3>
+                <div></div>
+              </li>
+              <li class="item">
+                <a href="/article/detail-175.html" target="_blank"><img src="https://www.lxtkj.cn/uploads/picture/20181113/d194394bca6ef99063c8ffe71928e3eb.jpg" width="335" height="200" alt="王者荣耀为什么在上分最后一把总容易遇到坑B？"></a>
+                <h3><a href="/article/detail-175.html" target="_blank" style="color:#FFF;text-decoration:none;">王者荣耀为什么在上分最后一把总容易遇到坑B？</a></h3>
+                <div></div>
+              </li>
+              </ul>
+          </div>
+          <div class="controler" data-scro="controler"> <b class="down">1</b> <b class="">2</b> <b class="">3</b> <b class="">4</b> </div>
+          <div class="controler2" data-scro="controler2"> <a href="javascript:;" class="prev"><i></i></a> <a href="javascript:;" class="next"><i></i></a> </div>
+        </div>
+      </article>
+
+      <!--排行榜-->
+      <article class="hot-box">
+        <div class="hot-box-title"> 点击排行
+          <div style="position: absolute; top: 8px; left: 185px"> <img border="0" src="https://www.lxtkj.cn/template/keji/static/picture/pic001.jpg" width="113" height="27"> </div>
+        </div>
+        <div id="tab">
+          <h3 class="up" id="two1" onmouseover="setContentTab('two',1,2)">今日点击</h3>
+          <h3 id="two2" onmouseover="setContentTab('two',2,2)" class="">本周点击</h3>
+          <div class="clear"></div>
+          <div class="block" id="con_two_1" style="display: block;">
+            <ul>
+              <li><span class="first">1</span>
+                <a class="tab_title" href="/article/detail-178.html">电脑网络连接中的以太网和本地连接有什么区别?</a>
+              </li>
+              <li><span class="num01">2</span>
+                <a class="tab_title" href="/article/detail-175.html">王者荣耀为什么在上分最后一把总容易遇到坑B？</a>
+              </li>
+              <li><span class="num01">3</span>
+                <a class="tab_title" href="/article/detail-11.html">京东再强又怎样 百度竞价排名坐地收钱无成本</a>
+              </li>
+              <li><span class="num01">4</span>
+                <a class="tab_title" href="/article/detail-154.html">中国互联网市场下沉 三四五线城市会出下一个BAT?</a>
+              </li>
+              <li><span class="num01">5</span>
+                <a class="tab_title" href="/article/detail-59.html">牛津大学开发VR成功治疗恐高症</a>
+              </li>
+              <li><span class="num01">6</span>
+                <a class="tab_title" href="/article/detail-43.html">苹果升级iPhone安全性：拒绝暴力破解</a>
+              </li>
+            </ul>
+          </div>
+          <div class="clear"></div>
+          <div id="con_two_2" style="display: none;">
+            <ul>
+              <li><span class="first">1</span>
+                <a class="tab_title" href="/article/detail-178.html">电脑网络连接中的以太网和本地连接有什么区别?</a>
+              </li>
+              <li><span class="num01">2</span>
+                <a class="tab_title" href="/article/detail-175.html">王者荣耀为什么在上分最后一把总容易遇到坑B？</a>
+              </li>
+              <li><span class="num01">3</span>
+                <a class="tab_title" href="/article/detail-11.html">京东再强又怎样 百度竞价排名坐地收钱无成本</a>
+              </li>
+              <li><span class="num01">4</span>
+                <a class="tab_title" href="/article/detail-154.html">中国互联网市场下沉 三四五线城市会出下一个BAT?</a>
+              </li>
+              <li><span class="num01">5</span>
+                <a class="tab_title" href="/article/detail-59.html">牛津大学开发VR成功治疗恐高症</a>
+              </li>
+              <li><span class="num01">6</span>
+                <a class="tab_title" href="/article/detail-43.html">苹果升级iPhone安全性：拒绝暴力破解</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </article>
+      <!--广告1-->
+      <div class="ad300">
+        xx
+      </div>
       </div>
     </div>
   </div>
@@ -62,6 +285,247 @@ export default {
   align-items: center;
   text-align: center;
 }
+.content {
+  border: 1px solid green;
+  display: flex;
+  width: 100%;
+  max-width: 1200px;
+  min-width: 320px;
+  margin: auto;
+  justify-content: space-between;
+}
+.content-left{
+  border: 1px solid red;
+  width: 25%;
+  /* height: 1500px; */
+}
+.content-right{
+  border: 1px solid red;
+  width: 35%;
+  /* height: 1500px; */
+}
+.content-aside{
+  border: 1px solid red;
+  width: 35%;
+  /* height: 1500px; */
+}
+.public-style {
+    position: relative;
+    border-top: 1px solid #e8e8e8;
+    box-sizing: border-box;
+}
+.public-style::before {
+    content: "";
+    display: block;
+    height: 2px;
+    width: 65px;
+    background-color: #004276;
+    position: absolute;
+    top: -1px;
+}
+.isgood {
+    box-sizing: border-box;
+    width: 240px;
+    height: auto;
+    margin-top: 20px;
+}
+.isgood .isgood-header {
+    padding: 10px 0 18px;
+    position: relative;
+    box-sizing: border-box;
+    font-size: 0;
+    text-align: left;
+}
+.isgood .isgood-header p {
+    display: inline-block;
+    font-size: 16px;
+    vertical-align: middle;
+    font-weight: 800;
+}
+.isgood .isgood-box {
+    height: 210px;
+    position: relative;
+    overflow: hidden;
+}
+.isgood .isgood-pic h3 {
+    font: bold 14px/30px Microsoft YaHei;
+}
+.isgood .isgood-pic h3 a {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: block;
+}
+.isgood .isgood-pic img {
+    margin: 2px 10px 4px 0px;
+    display: block;
+    float: left;
+}
+.isgood .isgood-pic P {
+    line-height: 20px;
+    color: #666;
+}
+
+.isgood .isgood-box ul {
+    width: 240px;
+    height: 223px;
+    overflow: hidden;
+    position: relative;
+}
+.isgood .isgood-box li a {
+    display: block;
+    width: 100%;
+    color: #404040;
+    font-size: 14px;
+    margin-top: 8px;
+    position: relative;
+    padding-left: 12px;
+    box-sizing: border-box;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-height: 1;
+}
+.isgood .isgood-box li a:before {
+    content: "";
+    width: 4px;
+    height: 4px;
+    background-color: #ccc;
+    border-radius: 4px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+}
+/* 上中样式 */
+.content-right .right-con {
+    height: auto;
+    float: left;
+    margin-top: 20px;
+    left: 0;
+    padding-bottom: 15px;
+    border-bottom: 1px dashed #ccc;
+    width: 100%;
+}
+.items ul li {
+    padding-left: 20px;
+    line-height: 32px;
+    position: relative;
+}
+.items .items-title {
+    font-weight: bold;
+}
+.items ul li::before {
+    width: 5px;
+    height: 5px;
+    display: block;
+    content: "";
+    background-color: #ccc;
+    border-radius: 5px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    left: 0;
+}
+.items ul a {
+    /* width: 400px; */
+    display: block;
+    color: #404040;
+    font-size: 16px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    text-align: left;
+}
+
+.news ul li {
+    padding-left: 20px;
+    line-height: 32px;
+    position: relative;
+}
+.news .news-title {
+    font-weight: bold;
+}
+.news ul li::before {
+    width: 5px;
+    height: 5px;
+    display: block;
+    content: "";
+    background-color: #ccc;
+    border-radius: 5px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    left: 0;
+}
+.news ul a {
+    /* width: 400px; */
+    display: block;
+    color: #404040;
+    font-size: 14px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    text-align: left;
+}
+.right-con .isgood-header {
+    padding: 10px 0 18px;
+    position: relative;
+    box-sizing: border-box;
+    font-size: 0;
+    text-align: left;
+}
+.right-con .isgood-header p {
+    display: inline-block;
+    font-size: 16px;
+    vertical-align: middle;
+    font-weight: 800;
+}
+
+/* 侧边样式 */
+.section-focus-pic {
+    margin: 20px auto 0 auto;
+    height: 200px;
+    width: 335px;
+    overflow: hidden;
+    position: relative;
+}
+.section-focus-pic .pages {
+    width: 335px;
+    height: 200px;
+}
+.section-focus-pic ul li {
+    position: absolute;
+    top: 0;
+    left: 335px;
+    width: 335px;
+    height: 200px;
+}
+.section-focus-pic ul li h3 {
+    position: absolute;
+    bottom: 11px;
+    left: 0;
+    width: 335px;
+    font-weight: normal;
+    color: #FFF;
+    margin-left: 10px;
+    z-index: 111;
+    font-size: 12px;
+    font-family: "微软雅黑";
+}
+.section-focus-pic ul li div {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 40px;
+    background-color: #000;
+    width: 335px;
+    opacity: 0.6;
+    filter: alpha(opacity=60);
+}
 
 /* .title {
   font-family:
@@ -87,9 +551,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
